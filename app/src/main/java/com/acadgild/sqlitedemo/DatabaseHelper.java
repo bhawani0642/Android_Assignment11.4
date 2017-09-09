b63ff68ca724f53548a2199f8a2a43fc9ee7bf9d
bhawani0642 <bhawani0642@gmail.com>
2017-09-09 11:08:13 +0530

    Android_Assignment

---
+++ app/src/main/java/com/acadgild/sqlitedemo/DatabaseHelper.java
@@ -0,0 +1,70 @@
+package com.acadgild.sqlitedemo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Pri on 8/21/2017.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "_id";
+    public static final String COL_2 = "firstname";
+    public static final String COL_3 = "lastname";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 2);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " ("+COL_1+" INTEGER PRIMARY KEY AUTOINCREMENT, "+COL_2+ " TEXT, " + COL_3 + " TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String firstName, String lastName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, firstName);
+        contentValues.put(COL_3, lastName);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
+
+    public boolean updateData(String id, String firstname, String lastname) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2,firstname );
+        contentValues.put(COL_3, lastname);
+
+        db.update(TABLE_NAME, contentValues, COL_1 + " = ?", new String[]{id});
+        return true;
+    }
+
+    public Integer deleteData(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, COL_1 + " = ?", new String[]{id});
+    }
+}
